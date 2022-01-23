34f2c34022dd961f5ad1f30317148ebc83c085a7
Christin B Koshy <christinkoshy1998@gmail.com>
2022-01-23 12:19:52 +0530

    fixed error

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,10 +2,6 @@ import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
 import { db } from "../../../firebase";
 import { FirebaseAdapter } from "@next-auth/firebase-adapter";
-import NextAuth from "next-auth"
-import Providers from "next-auth/providers"
-import { db } from "../../../firebase"
-import { FirebaseAdapter } from "@next-auth/firebase-adapter"
 
 export default NextAuth({
   // Configure one or more authentication providers
